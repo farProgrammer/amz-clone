6292bfb5cbd966c455afabecd94620fbc66feeea
farProgrammer <kfarprogrammer@gmail.com>
2021-08-21 16:59:31 -0500

    add

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/challenge-fed46/us-central1/api'//API 
+    // baseURL:'http://localhost:5001/challenge-fed46/us-central1/api'//API 
 });
 
 
